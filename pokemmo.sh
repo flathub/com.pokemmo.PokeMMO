da6bb3de4aef21e19b31cdcff79e6768020328c0
Kyu <kyu@pokemmo.com>
2024-11-19 19:51:40 -0600

    Packaging rewrite for PokeMMO Launcher * This supersedes the previous Flathub packaging * The PokeMMO Launcher now wraps the game client with optimized settings and manages downloads / signature verification of the client. This application also manages memory settings and client repairs * This changes the target download directory and fixes issues with settings / caches being deleted randomly. Users will be prompted to redownload the game on start

--- pokemmo.sh
+++ pokemmo.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-
-if [ ! -d $XDG_DATA_HOME/data ]
-then
-	cp -r /app/extra/data $XDG_DATA_HOME
-fi
-
-if [ ! -d $XDG_DATA_HOME/config ]
-then
-	cp -r /app/extra/config $XDG_DATA_HOME
-fi
-
-cd $XDG_DATA_HOME
-java -Xmx384M -Dfile.encoding="UTF-8" -Dorg.lwjgl.glfw.libname=/app/lib/libglfw.so -cp /app/extra/PokeMMO.exe com.pokeemu.client.Client "$@"
+export POKEMMO_IS_FLATPAKED=1 ; java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar
