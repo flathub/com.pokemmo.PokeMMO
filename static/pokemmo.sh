6b510cc08f6a2b78a1d3f94bf1419da0d77448dd
Kyu <kyu@pokemmo.com>
2025-09-05 22:02:06 -0500

    Update unix-launcher to 3.0d-stable (#35)
    * feat: Update unix-launcher to 3.0d-stable. See https://github.com/kyu-n/pokemmo-unix-installer/releases/tag/v3.0d-stable for release notes
    * feat: Enable Wayland, Fallback to x11
    * fix: Removed manual env exports (these are now populated / determined by the launcher)
    * chore: Update readme, notes
    * fix: Wayland desktop init hints
    * feat: Additional icon sizes
    * fix: Use StartupNotify for main desktop entry
    * fix: Spawn java with exec for shell pid retention

--- static/pokemmo.sh
+++ static/pokemmo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-export POKEMMO_IS_FLATPAKED=1 ; java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar "$@"
+exec java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar "$@"
