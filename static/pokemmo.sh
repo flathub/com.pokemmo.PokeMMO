de342f20c7ac51ff8e278d55f14b6d1218337202
Kyu <kyu@pokemmo.com>
2025-10-02 16:47:41 -0500

    Aarch64 compat test

--- static/pokemmo.sh
+++ static/pokemmo.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar "$@"
+
+if [ "$(uname -m)" = "aarch64" ]; then
+  exec java -Dfile.encoding="UTF-8" -Djava.library.path=/app/lib -jar /app/bin/unix-installer.jar "$@"
+else
+  exec java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar "$@"
+fi
