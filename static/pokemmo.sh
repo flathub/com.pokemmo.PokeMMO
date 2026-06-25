5b0b3f89580bb1cda277cda183623347aa8438f7
Desu <desu@pokemmo.eu>
2026-06-25 04:42:36 +0000

    feat: initial native-image build

--- static/pokemmo.sh
+++ static/pokemmo.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-if [ "$(uname -m)" = "aarch64" ]; then
-  exec java -Dfile.encoding="UTF-8" -Djava.library.path=/app/lib -jar /app/bin/unix-installer.jar "$@"
-else
-  exec java -Dfile.encoding="UTF-8" -jar /app/bin/unix-installer.jar "$@"
-fi
+export POKEMMO_IS_FLATPAKED=1
+exec /app/bin/launcher "$@"
